b97bee24b38dcf963ea0dbeffeb6ed547f81a6f7
Chen Lu <clu@dolby.com>
2022-02-22 08:34:14 +0800

    Quick bug fix.

--- shell/set_macos_mouse_scaling.sh
+++ shell/set_macos_mouse_scaling.sh
@@ -3,7 +3,7 @@
 scaling=$(defaults read .GlobalPreferences com.apple.mouse.scaling)
 echo "Current com.apple.mouse.scaling is: ${scaling}"
 
-if [ "-1" -ne "${scaling}" ]; then
+if [ "-1" != "${scaling}" ]; then
 	echo "Set com.apple.mouse.scaling to -1"
 	defaults write .GlobalPreferences com.apple.mouse.scaling -1
 fi
